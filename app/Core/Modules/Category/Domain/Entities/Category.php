a6bc71f0bf007e2f8c7c658f6edfdc3ca07439bf
Caique Daniel <caiquedaniel@outlook.com.br>
2023-07-29 22:04:16 -0300

    feat: adicionar entities

---
+++ app/Core/Modules/Category/Domain/Entities/Category.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Core\Modules\Category\Domain\Entities;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Category implements Arrayable
+{
+    private int $id;
+    private string $title;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id ?? null,
+            'title' => $this->title
+        ];
+    }
+
+    private function setId(int $value): self
+    {
+        $this->id = $value;
+        return $this;
+    }
+
+    public static function hydrate(int $id, string $title): static
+    {
+        $category = new Category();
+
+        return $category
+            ->setId($id)
+            ->setTitle($title);
+    }
+}
---
+++ app/Core/Modules/Post/Domain/Entities/Post.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Core\Modules\Post\Domain\Entities;
+
+use DateTime;
+use App\Core\Modules\Category\Domain\Entities\Category;
+use Illuminate\Contracts\Support\Arrayable;
+
+class Post implements Arrayable
+{
+    private int $id;
+    private string $title;
+    private string $subtitle;
+    private string $article;
+    private ?DateTime $publishedAt;
+    private ?Category $category;
+
+    public function isPublished(): bool
+    {
+        return $this->getPublishedAt() != null;
+    }
+
+    public function publish(): self
+    {
+        $this->publishedAt = new DateTime('now');
+        return $this;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getSubtitle(): string
+    {
+        return $this->subtitle;
+    }
+
+    public function getArticle(): string
+    {
+        return $this->article;
+    }
+
+    public function getPublishedAt(): ?DateTime
+    {
+        return $this->publishedAt;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setTitle(string $value): self
+    {
+        $this->title = $value;
+        return $this;
+    }
+
+    public function setSubtitle(string $value): self
+    {
+        $this->subtitle = $value;
+        return $this;
+    }
+
+    public function setArticle(string $value): self
+    {
+        $this->article = $value;
+        return $this;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id ?? null,
+            'title' => $this->title,
+            'subtitle' => $this->subtitle,
+            'article' => $this->article,
+            'category' => $this->category?->toArray()
+        ];
+    }
+
+    private function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    private function setPublishedAt(DateTime $value): self
+    {
+        $this->publishedAt = $value;
+        return $this;
+    }
+
+    public static function hydrate(
+        int       $id,
+        string    $title,
+        string    $subtitle,
+        string    $article,
+        ?DateTime $publisedAt,
+        ?Category $category
+    ): Post
+    {
+        $post = new Post();
+
+        return $post
+            ->setId($id)
+            ->setTitle($title)
+            ->setSubtitle($subtitle)
+            ->setArticle($article)
+            ->setPublishedAt($publisedAt)
+            ->setCategory($category);
+    }
+}
